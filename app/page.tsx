1051403d2d1d73230f325f0734961b5914f8a21a
sujan khatri <sujansince2059@gmail.com>
2024-05-16 08:50:48 +0545

    Basic Overview of Routing and Navigation:::: usecase of Link instead of anchor tag for navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <a href="/about" className="block">
+        About us
+      </a>
+      {/* avoid using anchor tag as it reload the
+      webpage and network requests need to be made again resulting loading time
+      of app high */}
+      <Link href={"/about/company"}>About company</Link>
+
+      {/* use Link instead of anchor tag it doesnot reload the application and only make network request required for that page we are going to navigate */}
     </div>
   );
 }
