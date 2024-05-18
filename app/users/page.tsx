8d12e646f02a26ee61c2d657e55e49e0940e0ee2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-05-18 18:18:11 +0545

    making separate component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,12 @@
 import React from "react";
+import UserTable from "./UserTable";
 
 const page = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-
-  //if we disable caching this component will be dynamically rendered as nextjs think the data changes but if the caching is enable this component is statically rendered
-  const users = await res.json();
-  console.log(users);
   return (
     <div>
       <h1>users list</h1>
-      <p>{new Date().toLocaleString()}</p>
-
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user: any) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {/* <p>{new Date().toLocaleString()}</p> */}
+      <UserTable />
     </div>
   );
 };
