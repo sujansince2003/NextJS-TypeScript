9b902e214b28cb5d35bd369ef318c86f923b642c
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-05-19 07:40:31 +0545

    programatic navigation

---
+++ app/newuser/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+const page = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>new user page</h1>
+      <button className="btn btn-ghost" onClick={() => router.push("/users")}>
+        create user
+      </button>
+    </div>
+  );
+};
+
+export default page;
