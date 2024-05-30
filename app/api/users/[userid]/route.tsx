f58d4c4f1b7beeb0ff3032261c72b873d541f883
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-05-30 16:29:25 +0545

    DELETE Request

--- app/api/users/[userid]/route.tsx
+++ app/api/users/[userid]/route.tsx
@@ -17,6 +17,7 @@ export async function PUT(
   { params }: { params: { userid: number } }
 ) {
   const body = await request.json();
+
   if (!body.name) {
     return NextResponse.json({ error: "name is required" }, { status: 400 });
   }
@@ -24,4 +25,23 @@ export async function PUT(
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   }
   return NextResponse.json({ id: 1, name: "hello sujan" });
+}
+
+//deleting the object
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { userid: number } }
+) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json(
+      { error: "sorry name is required" },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { success: "object deleted succesfully" },
+    { status: 200 }
+  );
 }
