ee394972c5ae5ee85b721b9b4f1c927bb7ef84f4
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-05-30 15:04:30 +0545

    PUT request

--- app/api/users/[userid]/route.tsx
+++ app/api/users/[userid]/route.tsx
@@ -9,3 +9,19 @@ export function GET(
 
   return NextResponse.json({ id: `${params.userid}`, name: "sujan k" });
 }
+
+// updating the object :: PUT request
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { userid: number } }
+) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  }
+  if (params.userid > 10) {
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  }
+  return NextResponse.json({ id: 1, name: "hello sujan" });
+}
