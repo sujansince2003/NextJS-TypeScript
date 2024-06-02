3fd64f160a793f00d9c28a44e18c4185572318f0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-02 21:02:30 +0545

    creting POST and GET request for products:: exercise

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,26 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import Productschema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  const obj = [
-    {
-      id: "milk",
-    },
-    {
-      id: "milk xd",
-    },
-  ];
+export async function GET(request: NextRequest) {
+  let data;
+  try {
+    data = await prisma.product.findMany();
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to retrieve products" },
+      { status: 500 }
+    );
+  }
+  if (data.length === 0) {
+    return NextResponse.json({ error: "data is empty" }, { status: 204 });
+  }
 
-  return NextResponse.json(obj);
+  return NextResponse.json(data, { status: 200 });
 }
 
 // creating post
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = Productschema.safeParse(body);
-  if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  }
-  return NextResponse.json(body, { status: 200 });
+  // const validation = Productschema.safeParse(body);
+  // if (!validation.success) {
+  //   return NextResponse.json(validation.error.errors, { status: 400 });
+  // }
+
+  const product = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+  return NextResponse.json(product, { status: 200 });
 }
