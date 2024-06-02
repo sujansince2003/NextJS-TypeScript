c0dcda56ca60977abf06a9c9c5d29eac02a2a085
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-02 17:15:35 +0545

    creating and reading data from database with prisma client

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,19 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import Userschema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
+  const data = await prisma.user.findMany();
+
   //here we are not using this request: NextRequest in code inside function but if we dont take it as params next will cache data,to prevent caching  we use request object
-  return NextResponse.json({
-    id: 1,
-    name: "sujan",
-  });
+  return NextResponse.json(data);
 }
 
 //creating POST request
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  if (!body.name)
+  if (!(body.email && body.username))
     return NextResponse.json({ error: "name is requierd" }, { status: 401 });
-  return NextResponse.json(body);
+
+  const user = await prisma.user.create({
+    data: {
+      username: body.username,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(user);
 }
