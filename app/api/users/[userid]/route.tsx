34715a265118030f9cfd027d14d23d2cdec4b54c
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-03 09:05:29 +0545

    usint Next-cloudinary to handle file uploads

--- app/api/users/[userid]/route.tsx
+++ app/api/users/[userid]/route.tsx
@@ -6,7 +6,7 @@ export function GET(
   request: NextRequest,
   { params }: { params: { userid: number } }
 ) {
-  if (params.userid > 10)
+  if (!params.userid)
     return NextResponse.json({ error: "user not found" }, { status: 404 });
 
   return NextResponse.json({ id: `${params.userid}`, name: "sujan k" });
