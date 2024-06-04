a3dae54a30dbed7d9f0a21cfddb173d1e042f927
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-04 23:55:49 +0545

    session provider wrapper to handle session in client side

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./Nav";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <body className={inter.className}>
-        <Nav />
-        <main> {children}</main>
+        <AuthProvider>
+          <Nav />
+          <main> {children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
