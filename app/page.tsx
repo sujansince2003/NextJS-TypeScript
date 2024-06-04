2abf24a05081282aa626072d4440512023bb55e2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-04 23:59:26 +0545

    handle session in server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./Components/ProductCard";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {session && session.user!.name}</h1>
       <a href="/about" className="block">
         About us
       </a>
@@ -13,9 +16,7 @@ export default function Home() {
       webpage and network requests need to be made again resulting loading time
       of app high */}
       <Link href={"/about/company"}>About company</Link>
-
       {/* use Link instead of anchor tag it doesnot reload the application and only make network request required for that page we are going to navigate */}
-
       <ProductCard />
     </div>
   );
