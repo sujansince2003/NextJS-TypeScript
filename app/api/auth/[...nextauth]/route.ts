682e35f168364d86fdb573c37935ae4f73abd6e5
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-05 17:29:16 +0545

    added credentials for login user with email and password

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,47 @@
 // Import necessary modules from next-auth
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/prisma/client";
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from  "bcrypt"
 
 // Define the NextAuth configuration
-export const options:NextAuthOptions = {
-  adapter:PrismaAdapter(prisma),
+export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Enter email" },
+        password: {
+          label: "Password",
+          type: "password ",
+          placeholder: "Enter password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+        if(!user) return null
+      const ispasswordCorrect=  await bcrypt.compare(credentials?.password ,user.hashedPassword!)
+     return  ispasswordCorrect ? user :null
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session:
-  {
-    strategy:"jwt"
-  }
+  session: {
+    strategy: "jwt",
+  },
 };
 
 // Create the NextAuth handler
@@ -25,4 +49,4 @@ const handler = NextAuth(options);
 
 // Export the handler. Typically, you'd export it as a default export for a specific route,
 // but if you're handling multiple routes or methods, you might need to adjust this.
-export { handler as GET,handler as POST };
+export { handler as GET, handler as POST };
