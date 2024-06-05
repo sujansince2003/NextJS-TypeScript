b8e572fab9469606b77f47447166d662906266ec
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-05 17:00:13 +0545

    added session strategy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,10 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/prisma/client";
+import { NextAuthOptions } from "next-auth";
 
 // Define the NextAuth configuration
-export const options = {
+export const options:NextAuthOptions = {
   adapter:PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -13,6 +14,10 @@ export const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session:
+  {
+    strategy:"jwt"
+  }
 };
 
 // Create the NextAuth handler
